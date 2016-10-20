e87ddcdc22cf3c10642dfd9158e2369ff371b6ac
ikloev32 <ikloev32@naver.com>
2016-10-20 17:05:02 +0900

    주석 및 삭제코드 수정
    주석 및 삭제코드 수정

--- js/app.js
+++ js/app.js
@@ -4,22 +4,14 @@ $(function (){
 	//초기에 json 업로드
 	jsonupload();
 	
-	//사용할 변수명
-		//arr  = 선택한 제품 정보 저장
-		//id = 선택한 제품 id
-		//name = 선택한 제품 name
-		//price = 선택한 제품 price
-		//detail = 선택한 제품 정보
-		//ok = 있는지 없는지 확인
-		//no = 있을 경우 index번호
-		//count = 개수
-		//newprice = 개수 * 가격
-		//con2 = 삽입할 정보
-		//realtotal =  상품 가격에 쉼표 더한것
-		//addrealtotal = 총 가격에 쉼표 제거
+		
+		
 	arr = new Array();
+	
+	//드롭 후의 설정
 	$("#cartbox").droppable({
 		drop:function (event, ui){
+			//끌고온 것의 정보를 저장함
 			var id = ui.draggable.data("id")
 			var name = ui.draggable.context.children[0].innerHTML;
 			var price = ui.draggable.context.children[1].innerHTML;
@@ -27,14 +19,20 @@ $(function (){
 			var ok = true;
 			var no;
 			var count = 1;
+
+			//현재 이것이 이미 업로드 한 것인지 확인
 			for(var k=0; k<arr.length; k++){
 				if(arr[k][0] == name){
 					ok = false;
 					no = k;
 				}
 			}
+			
+			//없다면 넣고
 			if(ok==true){
 				arr.push([name, price, detail,count, id,price]);	
+				
+			//있으면 소개와 구매수량을 바꿔줌
 			}else{
 				count = arr[no][3];
 				count ++;
@@ -43,6 +41,8 @@ $(function (){
 				var total = newprice * arr[no][3];
 				arr[no][5] = total;
 			};
+			
+			//정보를 모두 정리했으면 넣어줌
 			var con2  = "";
 			for(var g=0; g<arr.length; g++){
 				con2 += '<tr>';
@@ -51,44 +51,13 @@ $(function (){
 				con2 += '	<th>'+arr[g][2]+'</th>';
 				con2 += '<th>'+arr[g][1]+'</th>';
 				con2 += '<th>'+arr[g][3]+'</th>';
+				//콤마 넣어주는 것
 				realtotal  = comma(arr[g][5]);
 				con2 += '<th>'+realtotal+'</th>';
-				con2 += '<th><button class="delete" value="'+arr[g][4]+'">삭제</button></th>';
+				con2 += '<th><button class="delete" onClick="deletenow('+arr[g][4]+')">삭제</button></th>';
 				con2 += '</tr>';
 			}
 			$(".table-bordered tbody").html(con2);
-			
-			$(".delete").on('click', function (){
-				$(this).val();
-				for(var t=0; t<arr.length; t++){
-					if(arr[t][4] == $(this).val()){
-						arr.splice(t,1);
-						console.log(arr);
-						var con2  = "";
-						for(var g=0; g<arr.length; g++){
-							con2 += '<tr>';
-							con2 += '<th>'+arr[g][4]+'</th>';
-							con2 += '<th>'+arr[g][0]+'</th>';
-							con2 += '	<th>'+arr[g][2]+'</th>';
-							con2 += '<th>'+arr[g][1]+'</th>';
-							con2 += '<th>'+arr[g][3]+'</th>';
-							realtotal  = comma(arr[g][5]);
-							con2 += '<th>'+realtotal+'</th>';
-							con2 += '<th><button class="delete" value="'+arr[g][4]+'">삭제</button></th>';
-							con2 += '</tr>';
-						}
-						$(".table-bordered tbody").html(con2);
-						var realtotal = 0;
-						for(var g=0; g<arr.length; g++){
-							var addrealtotal = uncomma(arr[g][5]);
-							addrealtotal = Number(addrealtotal);
-							realtotal += addrealtotal;
-						}
-						realtotal  = comma(realtotal);
-						$("#total").html(realtotal+"원")
-					}
-				}
-			});
 			var realtotal = 0;
 			for(var g=0; g<arr.length; g++){
 				var addrealtotal = uncomma(arr[g][5]);
@@ -101,17 +70,42 @@ $(function (){
 	});	
 });
 
-function deletee(val){
-	console.log(arr[0][4]);
-	console.log(val);
+
+
+function deletenow(val){
 	for(var t=0; t<arr.length; t++){
-		if(arr[t][4] == $(this).val()){
-			arr.split(t,1);
+		if(arr[t][4] == val){
+			arr.splice(t,1);
 			console.log(arr);
+			var con2  = "";
+			for(var g=0; g<arr.length; g++){
+				con2 += '<tr>';
+				con2 += '<th>'+arr[g][4]+'</th>';
+				con2 += '<th>'+arr[g][0]+'</th>';
+				con2 += '	<th>'+arr[g][2]+'</th>';
+				con2 += '<th>'+arr[g][1]+'</th>';
+				con2 += '<th>'+arr[g][3]+'</th>';
+				//콤마 넣어주는 것
+				realtotal  = comma(arr[g][5]);
+				con2 += '<th>'+realtotal+'</th>';
+				con2 += '<th><button class="delete" onClick="deletenow('+arr[g][4]+')">삭제</button></th>';
+				con2 += '</tr>';
+			}
+			$(".table-bordered tbody").html(con2);
+			var realtotal = 0;
+			for(var g=0; g<arr.length; g++){
+				var addrealtotal = uncomma(arr[g][5]);
+				addrealtotal = Number(addrealtotal);
+				realtotal += addrealtotal;
+			}
+			realtotal  = comma(realtotal);
+			$("#total").html(realtotal+"원");
 		}
 	}
 }
 
+
+//기타 function들
 function uncomma(str) {
     str = String(str);
     return str.replace(/[^\d]+/g, '');
